cdbee7f6e485f8ed981fc5d8941c9c4cd9f24783
RDeleglise <deleglise.remy@laposte.net>
2016-09-29 15:18:40 +0200

    Main
    Première optimisation

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,42 +12,18 @@ import game.Room;
 public class Main {
 	
 	public static Room tmpRoom;
-	public static int indice = 1;
 	public static Room salleTest;
 	
 	public static void main(String[] args) {
-			List <Room> tmpList = new ArrayList<Room>();
-			int taille = 5, i = 0;
-			
-			//Création première salle
-	tmpRoom = new Room("Salle 1");
-	
-	for(int j = 0 ; j < taille ; j++) {
+		List <Room> tmpList = new ArrayList<Room>();
+		int taille = 5, i = 0;
+		int indice = 1;
 		
-		//Création première ligne
-		if(j == 0) {
-			for(i = 0 ; i < taille ; i++) {
-				
-				//Création de la salle suivante
-				Room nextRoom = new Room("Salle " + (indice+1));
-								
-				if(i != taille - 1)
-					tmpRoom.addNeighboors(Direction.Est, nextRoom);
-				tmpList.add(tmpRoom);
-				
-				if(i != taille - 1)
-					nextRoom.addNeighboors(Direction.Ouest, tmpRoom);
-								
-				tmpRoom = nextRoom;
-				
-				//Ajout de la dernière colonne
-				if (i == taille - 1) {
-					tmpList.add(tmpRoom);							
-				}						
-				indice ++;
-			}
-		}
-		else {
+		//Création première salle
+		tmpRoom = new Room("Salle 1");
+		
+		for(int j = 0 ; j < taille ; j++) {
+			
 			for(i = 0 ; i < taille ; i++) {
 				//Création de la salle suivante
 				Room nextRoom = new Room("Salle " + (indice+1));
@@ -57,43 +33,68 @@ public class Main {
 					tmpRoom.addNeighboors(Direction.Est, nextRoom);
 				}
 				
-				
-				//Ajout voisin nord grâce à la liste
-				tmpRoom.addNeighboors(Direction.Nord, tmpList.get(i));
-				
-				//Récupération de la salle dans la liste pour ajouter la salle en cours en voisin
-				tmpList.get(i).addNeighboors(Direction.Sud, tmpRoom);
-				
-				//Ajout de la salle en cours dans la liste, au bon indice
-				tmpList.set(i, tmpRoom);
-				
-				//Ajout voisin 
+				//Ajout voisin Ouest
 				if(i != taille - 1)
 					nextRoom.addNeighboors(Direction.Ouest, tmpRoom);
 				
-				//test
-				if(j == 4 && i == 0)
+				//Ajout voisin nord grâce à la liste
+				if(j != 0)
+					tmpRoom.addNeighboors(Direction.Nord, tmpList.get(i));
+				
+				//Récupération de la salle dans la liste pour ajouter la salle en cours en voisin
+				if(j != 0)
+					tmpList.get(i).addNeighboors(Direction.Sud, tmpRoom);
+				
+				//Ajout de la salle en cours dans la liste, au bon indice
+				if(j == 0)
+					tmpList.add(tmpRoom);
+				else						
+					tmpList.set(i, tmpRoom);
+				
+				//Test
+				if(j == 3 && i == 3)
 					salleTest = tmpRoom;
 					
 				tmpRoom = nextRoom;
 				
-				//Ajout de la dernière colonne
-				if (i == taille - 1) {
-					tmpList.add(tmpRoom);							
-				}	
-				
-				
 				indice ++;
-			}
+			
+			}			
 		}
+
+		
+		System.out.println("Salle en cours : " + salleTest.getDescription());
+		
+		
+		//Affichage salle Sud
+		try {
+			System.out.println("Salle au Sud : " + salleTest.getNeighboors(Direction.Sud).getDescription());
+		} catch (Exception e) {
+			System.out.println("Pas de salle au Sud");
+		}
+		
+		//Affichage salle Nord
+		try {
+			System.out.println("Salle au Nord : " + salleTest.getNeighboors(Direction.Nord).getDescription());
+		} catch (Exception e) {
+			System.out.println("Pas de salle au Nord");
+		}
+		
+		//Affichage salle Est
+		try {
+			System.out.println("Salle a l'Est : " + salleTest.getNeighboors(Direction.Est).getDescription());
+		} catch (Exception e) {
+			System.out.println("Pas de salle à l'Est");
+		}
+		
+		//Affichage salle Ouest
+		try {
+			System.out.println("Salle a l'Ouest : " + salleTest.getNeighboors(Direction.Ouest).getDescription());
+		} catch (Exception e) {
+			System.out.println("Pas de salle à l'Ouest");
+		}
+		
 	}
-	System.out.println("Salle en cours : " + salleTest.getDescription());
-	//System.out.println("Salle au Sud : " + salleTest.getNeighboors(Direction.Sud).getDescription());
-	System.out.println("Salle au Nord : " + salleTest.getNeighboors(Direction.Nord).getDescription());
-	System.out.println("Salle a l'Est : " + salleTest.getNeighboors(Direction.Est).getDescription());
-	System.out.println("Salle a l'Ouest : " + salleTest.getNeighboors(Direction.Ouest).getDescription());
-	
-}
 	
 
 }
