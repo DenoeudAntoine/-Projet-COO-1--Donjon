087bf0feb289363195750427024cd66fd39ac6d8
DenoeudAntoine <denoeudantoine@laposte.net>
2016-09-29 14:38:26 +0200

    Move

--- src/action/Move.java
+++ src/action/Move.java
@@ -2,25 +2,37 @@ package action;
 
 import character.Player;
 import game.Direction;
+import game.Game;
 import game.Room;
 
 public class Move extends Action {
 
-	public Move(Room currentRoom,Direction d) {
+	protected Direction d;
+	protected Game g;
+	
+	public Move(Game g, Room currentRoom,Direction d) {
 		// TODO Auto-generated constructor stub
 		super(currentRoom);
+		this.d = d;
+		this.g = g;
 	}
 
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Tu vas être déplacé");
+		g.playerMoveTo(d);
 	}
 
 	@Override
 	public boolean isPossible(Player p) {
 		// TODO Auto-generated method stub
-		return true;
+		if ( currentRoom.getNeighboors(d) != null) {
+			p.addAction(this);
+			return true;
+		}
+			
+		return false;
 	}
 
 }
