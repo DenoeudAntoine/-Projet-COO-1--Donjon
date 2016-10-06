d7237972d04eafba7c2961001a566db27d06340a
RDeleglise <deleglise.remy@laposte.net>
2016-10-06 18:21:01 +0200

    Update Monster.java

--- src/character/Monster.java
+++ src/character/Monster.java
@@ -1,16 +1,19 @@
 package character;
 
 public class Monster extends GameCharacter {
-		
+	/**
+	 * Monster constructor
+	 * @param hp - Character's life
+	 * @param strength - Character's strength
+	 * @param coins - Number of coins of character
+	 */
 	public Monster(int hp, int strength, int coins) {
 		super(hp, strength, coins);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return "Mosntre : " + hp + " hp / " + strength + " force / " + coins + " coins";
+		return "Monstre : " + hp + " hp / " + strength + " force / " + coins + " coins";
 	}
 	
 }
