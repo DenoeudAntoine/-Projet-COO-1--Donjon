046d7b423d121884e4121ab9bdeded09190c6d30
RDeleglise <deleglise.remy@laposte.net>
2016-10-06 18:24:51 +0200

    Update Player.java

--- src/character/Player.java
+++ src/character/Player.java
@@ -10,64 +10,91 @@ import scanner.ScannerInt;
 import action.Action;
 
 public class Player extends GameCharacter {
-	//protected List<Item> lstItems = new ArrayList<Item>();
-	
+	/**
+	 * lActions attribute corresponding to the action's list of character
+	 */
 	protected List<Action> lActions = new ArrayList<Action>();
 	
 	
+	/**
+	 * Player constructor
+	 * @param hp - Character's life
+	 * @param strength - Character's strength
+	 * @param coins - Number of coins of character
+	 */
 	public Player(int hp, int strength, int coins) {
 		super(hp, strength, coins);
 		// TODO Auto-generated constructor stub
 	}
 	
+	
+	/**
+	 * lActions getter
+	 * @return action's list of character
+	 */
 	public List<Action> getActions() {
 		return lActions;
 	}
 	
-	/*public List<Item> getItems() {
-		return lstItems;
-	}*/
 	
+	/**
+	 * Remove all actions in lActions
+	 */
 	public void removeAll() {
 		lActions = new ArrayList<Action>();
 	}
 	
-	public void heal(int hp) {
-		
+	
+	/**
+	 * Heal the player
+	 * @param hp - Number of points to heal
+	 */
+	public void heal(int hp) {		
 		this.hp += hp;
 	}
 	
+	
+	/**
+	 * Add coins to the player
+	 * @param coins - Number of coins to add
+	 */
 	public void addGolds(int coins){
 		this.coins += coins;
 	}
 	
+	
+	/**
+	 * Add strenght to the player
+	 * @param strength - Number of strength's points to add
+	 */
 	public void addStregth(int strength) {
 		this.strength += strength;
 	}
-	
-	/*public void addItem(Item item){
-		lstItems.add(item);
-	}*/
-	
-	public int getCoins() {
-		return coins;
-	}
-	
-	
-	
-	public void act() {
-		Action action = this.chose(lActions);
-        action.execute(this);
-	}
+		
 	
 	/**
-	 * @param a
+	 * Permit to chose action in lActions and execute it
+	 */
+	public void act() {
+		Action action = this.chose(lActions);
+        	action.execute(this);
+	}
+	
+	
+	/**
+	 * Add action in lActions
+	 * @param a - Action to add
 	 */
 	public void addAction(Action a) {
 		lActions.add(a);
 	}
 	
-
+	
+	/**
+	 * Permit to show actions's list and get the chosen action
+	 * @param liste - List that contains what to choose
+	 * @return chosen object
+	 */
 	public <T> T chose(List <T> liste) {
 		int i = 0;
 		do {
@@ -82,13 +109,11 @@ public class Player extends GameCharacter {
 				return null;
 		} while(i < 0 && i >= liste.size());
 		 
-		return liste.get(i-1);
-	
+		return liste.get(i-1);	
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return "Player 1 : " + hp + " HP / " + strength + "points de force / " + coins + " pieces d'or" ;
 	}
 }
