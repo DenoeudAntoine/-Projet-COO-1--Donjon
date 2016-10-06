087b2417b393f60038e570098497adbefc4b4b9a
RDeleglise <deleglise.remy@laposte.net>
2016-10-06 18:35:57 +0200

    Update Direction.java

--- src/game/Direction.java
+++ src/game/Direction.java
@@ -6,6 +6,11 @@ import java.util.List;
 public enum Direction {
 	Nord,Sud,Est,Ouest;
 	
+	
+	/**
+	 * Create a direction list and add all direction in this list
+	 * @return List of directions
+	 */
 	public static List<Direction> getAllDirection() {
 		List<Direction> lDirection = new ArrayList<Direction> ();
 		for(Direction d : Direction.values())
@@ -14,6 +19,12 @@ public enum Direction {
 		return lDirection;
 	}
 	
+	
+	/**
+	 * Return a string from last direction parameter
+	 * @param d - A direction
+	 * @return A string on the direction
+	 */
 	public String toString(Direction d) {
 		switch(d) {
 		case Nord:
@@ -21,14 +32,11 @@ public enum Direction {
 		case Sud:
 			return "au Sud.";
 		case Est:
-			return "� l'Est.";
+			return "a l'Est.";
 		case Ouest:
-			return "� l'Ouest.";
+			return "a l'Ouest.";
 		default:
 			return "le gps est dans le brouillard.";
-				
 		}
 	}
-	
- 
 }
