8140e789fb8e0fd3a3a4513acac8197c9fa2658f
RDeleglise <deleglise.remy@laposte.net>
2016-10-10 18:39:57 +0200

    Update Use.java
    JavaDoc

--- src/action/Use.java
+++ src/action/Use.java
@@ -6,37 +6,45 @@ import game.Room;
 import item.Item;
 
 public class Use extends Action {
-		
+	/**
+	 * Use constructor
+	 * @param game - Currently game
+	 */
 	public Use(Game game) {
-		// TODO Auto-generated constructor stub
 		super(game);
 	}
-
+	
+	
+	/**
+	 * This method permit to use a item
+	 * @param p - Player
+	 */
 	@Override
-	public void execute(Player p) {
-		
-		// choisir un item parmi les itesm de la piece
+	public void execute(Player p) {		
+		//Choose a item among the item in the room
 		Item item = p.chose(game.getCurrentRoom().getlItems());
-		// TODO Auto-generated method stub
+		
 		System.out.println("L'objet " + item + "va vous affecter !");
 		item.effect(p);
 		
-		game.getCurrentRoom().removeItem(item);
-		
+		game.getCurrentRoom().removeItem(item);		
 	}
-
+	
+	
+	/**
+	 * This method permit to check if there are still items in the room 
+	 * @return true if there are still items in the room / false otherwise
+	 */
 	@Override
 	public boolean isPossible() {
-		// ? this.game.getCurrentRoom().litems est vide ?
-		// TODO Auto-generated method stub
 		if (game.getCurrentRoom().getlItems().isEmpty())
 			return false;
 		return true;
 	}
 
+	
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return "Action use";
 	}
 
