c0d308af4619664a648bf46660496fb1593797fe
RDeleglise <deleglise.remy@laposte.net>
2016-10-10 18:47:59 +0200

    Update Gold.java
    JavaDoc

--- src/item/Gold.java
+++ src/item/Gold.java
@@ -5,21 +5,30 @@ import java.util.Random;
 import character.Player;
 
 public class Gold implements Item {
-	
+	/**
+	 * coins attribute corresponding to item coin
+	 */
 	protected int coins;
 	
+	/**
+	 * Gold constructor
+	 */
 	public Gold() {
-		// TODO Auto-generated constructor stub
 		Random r = new Random();
 		coins = r.nextInt(19) + 1;
 	}
-
+	
+	
+	/**
+	 * This method permit to add coins to a player
+	 * @param p - Player
+	 */
 	public void effect(Player p) {
-		// TODO Auto-generated method stub
-		System.out.println(coins + " coins vont �tre ajout� � la bourse !");
+		System.out.println(coins + " pieces vont etre ajoute a la bourse !");
 		p.addGolds(coins);
 	}
 
+	
 	public String toString() {
 		return coins + " pieces d'or";
 	}
