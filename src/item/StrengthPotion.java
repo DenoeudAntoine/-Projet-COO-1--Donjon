7b1b59f69f714fbecf52954e1cdd8690de472593
RDeleglise <deleglise.remy@laposte.net>
2016-10-10 19:11:38 +0200

    Update StrengthPotion.java
    JavaDoc

--- src/item/StrengthPotion.java
+++ src/item/StrengthPotion.java
@@ -5,24 +5,30 @@ import java.util.Random;
 import character.Player;
 
 public class StrengthPotion implements Item {
-
+	/**
+	 * strength attribute corresponding to the strength to add to a player
+	 */
 	protected int strength;
 	
+	/**
+	 * StrengthPotion constructor
+	 */
 	public StrengthPotion() {
-		// TODO Auto-generated constructor stub
 		Random r = new Random();
 		strength = r.nextInt(5) +1;
 	}
-
+	
+	
+	/**
+	 * This method permit to add strength to a player
+	 * @param p - Player
+	 */
 	public void effect(Player p) {
-		// TODO Auto-generated method stub
-		System.out.println(strength + " points de force sont ajout�s");
+		System.out.println(strength + " points de force sont ajoutes");
 		p.addStregth(strength);
 	}
 	
 	public String toString() {
 		return "Potion ajoutant " + strength + " points de force";
 	}
-
-
 }
