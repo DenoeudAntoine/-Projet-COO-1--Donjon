a849e1772b99b6c75de02f3dafe5df463b54d36e
DenoeudAntoine <denoeudantoine@laposte.net>
2016-10-10 19:22:10 +0200

    testPlayer

--- src/test/testPlayer.java
+++ src/test/testPlayer.java
@@ -16,12 +16,16 @@ public class testPlayer extends testGameCharacter {
 		return new Player(10,10,10);
 	}
 	
+	
+	/**
+	 * @see test.testGameCharacter
+	 */
 	@Test
 	@Override
 	public void testIsAttacked() {
 		// TODO Auto-generated method stub
 		Monster m = new Monster(1, 1, 1);
-		g1.isAttacked(m)
+		g1.isAttacked(m);
 		assertEquals(g1.getHP(),9);
 
 	}
